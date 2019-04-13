96edee490b75724eb1b884f0904ff206f5977404
Neakit <pavliukny@gmail.com>
2019-04-13 21:32:39 +0300

    add montserrat font css

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,7 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat:400,500,600&amp;subset=cyrillic' },
       {
         rel: 'stylesheet',
         href:
